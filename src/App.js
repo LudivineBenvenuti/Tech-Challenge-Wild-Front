084d43b2e40c7809bbf7ac5f89b89fc0ec7896f5
Ludivine BENVENUTI <ludivine.benvenuti@hotmail.fr>
2021-10-27 11:46:47 +0200

    Tech Challenge fait pour l'Alternance.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect} from "react";
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import "./App.css";
+
+function App () {
+    const  [argonaute, setArgonaute] = useState([])
+    const  [newMember, setNewMember] = useState('')
+
+const getDataArgonaute = async () => {
+    const resData = await axios.get('http://localhost:4242/argonaute')
+    console.log(resData)
+    setArgonaute(resData.data)
 }
+
+useEffect(() => {
+
+getDataArgonaute()
+}, []
+)
+
+const postData = async (e) => {
+  e.preventDefault()
+  const resData = await axios.post('http://localhost:4242/argonaute',{
+newMember
+  })
+  console.log(newMember)
+  getDataArgonaute()
+}
+
+return(
+<div className="App">
+<header>
+  <h1>
+    <img 
+    src="https://www.wildcodeschool.com/assets/logo_main-e4f3f744c8e717f1b7df3858dce55a86c63d4766d5d9a7f454250145f097c2fe.png" alt="Wild Code School logo" />
+    Les Argonautes
+  </h1>
+</header>
+
+
+<main>
+  <h2>Ajouter un(e) Argonaute</h2>
+  <form onSubmit={postData} className="new-member-form">
+    <label for="name">Nom de l&apos;Argonaute</label>
+    <input 
+    onChange={(e) => setNewMember(e.target.value)}
+    id="name" 
+    name="name" 
+    type="text" 
+    placeholder="Charalampos" />
+    <button type="submit">Envoyer</button>
+  </form>
+  
+  <h2>Membres de l'équipage</h2>
+  <section className="member-list">
+    {argonaute.map((argonaute) => (
+      <div key={argonaute.id} className="argonaute-item">
+        {argonaute.name}
+      </div>
+    ))}
+  </section>
+</main>
+
+<footer>
+  <p>Réalisé par Jason en Anthestérion de l'an 515 avant JC</p>
+</footer>
+</div>
+)}
 
 export default App;
